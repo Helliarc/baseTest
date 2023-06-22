3aef6f4e081431b99a71c98859f79918a31cc2a3
Helliarc <Helliarc@hotmail.com>
2023-06-22 00:29:42 +0000

    first change

--- baseTest/settings.py
+++ baseTest/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['helliarc.com']
+ALLOWED_HOSTS = [env("HOST_ALLOWED")]
 
 
 # Application definition
